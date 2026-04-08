a3e41467ff86a8cd6689f678703c5e8bbedaf739
SachinShah25 <sachinshah25082001@gmail.com>
2026-04-08 17:36:51 +0530

    third

--- src/Components/HeroRightSection.jsx
+++ src/Components/HeroRightSection.jsx
@@ -7,10 +7,8 @@ export const HeroRightSection=()=>{
 
     return <div className="hero_section_right">
         <img src={react} className="tech_icons t1" ></img>
-        <img src={mongo} className="tech_icons t2" ></img>
         <img src={exprss} className="tech_icons t3" ></img>
-        <img src={node} className="tech_icons t4" ></img>
-        <div className="over_div">
+            <div className="over_div">
             <div className="inner_div">
                 <div className="my_image">
                     <img src={myimage}/>
